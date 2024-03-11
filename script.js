adf2c42f65b1b3d15cf44870376bef6178da7fc5
Sanika Patil <patilsanika199@gmail.com>
2024-03-11 11:07:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const body = document.querySelector("body")
+ sidebar = body.querySelector(".sidebar"),
+ toggle = body.querySelector(".toggle"),
+ searchBtn = body.querySelector(".searchBtn"),
+ modeSwitch= body.querySelector(" .modeSwitch"),
+ modeText = body.querySelector(".modeText");
+
+
+ toggle.addEventListener("click", () =>{
+    sidebar.classList.toggle("close");
+ });
+ toggle.addEventListener("click", () =>{
+    sidebar.classList.remove("close");
+ });
+
+
+ modeSwitch.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light Mode"
+    }else{
+        modeText.innerText = "Dark Mode"
+    }
+ });
