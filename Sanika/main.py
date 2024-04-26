aa612ddd19e1bc45986aa79b69af13f7071021b9
Sanika Patil <patilsanika199@gmail.com>
2024-04-26 15:00:12 +0530

    updated

--- Sanika/main.py
+++ Sanika/main.py
@@ -1,19 +1,37 @@
 from flask import Flask , render_template ,jsonify, request
+from flask_pymongo import PyMongo
+import certifi
+import ssl
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Chatbot"
+mongo = PyMongo(app)
 
 app = Flask(__name__, template_folder='template')
 
 @app.route("/")
 def home():
-        return render_template("index.html")
+        chats = mongo.db.chats.find({})
+        myChats=[chat for chat in chats]
+        print(myChats)
+        return render_template("index.html", myChats=myChats)
 
 @app.route("/api", methods=["GET","POST"])
 def qa():
         if request.method == "POST":
                 print(request.json)
                 question = request.json.get("question")
-                data = {"result":f"Answer of {question}" } 
-                return jsonify(data)
-        data = {"result":"Hey i am sanika "}
+                chat = mongo.db.chats.find_one({"question":question})
+                print(chat)
+                if chat :
+                        data={"result": f"{chat['answer']}"}
+                        return jsonify(data)
+                else:
+                        data = {"result":f"Answer of {question}" } 
+                        mongo.db.chats.insert_one({"question":question, "answer": f"Answer from openai for {question}"})
+                        return jsonify(data)
+        data = {"result":"Hey i am chatbot "}
         return jsonify(data)        
        
     
--- Sanika/static/js/script.js
+++ Sanika/static/js/script.js
@@ -23,8 +23,6 @@ const body = document.querySelector("body")
         modeText.innerText = "Dark Mode"
     }
 
-   
-
     
 
  });
